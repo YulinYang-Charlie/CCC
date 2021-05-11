66cf2f70be9df9d8c11d4805e19a43209f0464f7
Yifan ZHAO <yifan.zhao4@student.unimelb.edu.au>
2021-05-11 14:45:18 +0800

    Update Doughnut.js

--- frontend/src/charts/Doughnut.js
+++ frontend/src/charts/Doughnut.js
@@ -1,44 +1,49 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
-const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
 
 class DoughnutChart extends React.Component{
   constructor(props){
     super(props)
     this.state={
+      loading:true,
       data:{},
-      labels:[],
-      datasets:[],
+      chartData:{
+        labels:[],
+        datasets:
+        [
+          {
+          label: '# of tweets',
+          data: [],
+          backgroundColor: [
+           'rgba(255, 99, 132, 0.2)',
+           'rgba(54, 162, 235, 0.2)',
+           'rgba(255, 206, 86, 0.2)',
+           'rgba(75, 192, 192, 0.2)',
+           'rgba(153, 102, 255, 0.2)',
+           'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+           'rgba(255, 99, 132, 1)',
+           'rgba(54, 162, 235, 1)',
+           'rgba(255, 206, 86, 1)',
+           'rgba(75, 192, 192, 1)',
+           'rgba(153, 102, 255, 1)',
+           'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+          }
+        ]
+      },
       arr:[],
     }
   }
-  componentDidMount(){
+  componentWillMount(){
+    console.log('willMount called')
+    this.getData()
+  }
+
+  getData(){
     fetch('./testdata.json',{
       method:'GET',
       headers : { 
@@ -50,31 +55,36 @@ class DoughnutChart extends React.Component{
     })
      .then(res =>res.json())
      .then((data) => {
-       console.log(data)  
+       console.log('fetch data received')
        this.setState({
          data:data
        },function(){
-         console.log(this.state.data)
-        //  let com = this.state.data.retBody.map((item,index)=>{
-        //    console.log(item.id)
-        //    return <li key={index}>{item.name}</li>
-        //  })
-        //  this.setState({
-        //    arr : com
-        //  },function(){
-        //    console.log(this.state.arr)
-        //  })
+        console.log('start setting states')
+         this.state.data.forEach(element => {
+           this.state.chartData.labels.push(element.name)
+           this.state.chartData.datasets[0].data.push(element.total)
+           this.setState({
+             loading:false
+           })
+         });
        })
      }) 
-  }
+    }
 
   render(){
-    return (
+    const datas = this.state.chartData
+    console.log('start rendering')
+    if (this.state.loading) return (
+      <div>
+        Loading...
+      </div>
+    )
+    else return (
       <>
       <div className='header'>
         <h1 className='title'>Doughnut Chart</h1>
       </div>
-      <Doughnut data={data} />
+      <Doughnut data={datas} />
     </>
     )
   }
