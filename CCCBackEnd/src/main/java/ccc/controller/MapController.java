337337fe84dc69567a2d83b9af04114c2c7816c5
sumengzhang <sumengz@student.unimelb.edu.au>
2021-05-14 10:55:00 +0800

    Backend_demo_submit

--- CCCBackEnd/src/main/java/ccc/controller/MapController.java
+++ CCCBackEnd/src/main/java/ccc/controller/MapController.java
@@ -1,7 +1,10 @@
 package ccc.controller;
 
 import ccc.entity.Region;
+import ccc.pojo.Sofa;
 import ccc.service.ChartService;
+import org.ektorp.CouchDbConnector;
+import org.ektorp.Options;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +23,9 @@ public class MapController {
 
     @Autowired
     private ChartService chartService;
+
+    @Autowired
+    private CouchDbConnector couchDbConnector;
     /**
      *  return pure statistics for all regions
      * @return
@@ -28,6 +34,17 @@ public class MapController {
     public List<Region> getPureStatistics(){
         return chartService.getPureStatistics();
     }
+
+
+    /**
+     * Test the database connection
+     * @return
+     */
+    @RequestMapping(value = "getSofa",method = RequestMethod.GET )
+    public Sofa getSofa() throws Exception {
+        String id = "b758e336598707a7f136e6bf100937d1";
+        Sofa sofa =  couchDbConnector.find(Sofa.class,id);
+        return sofa;
 
 
 }
