f83b3e6b89923cb0e1a60722666b8b433e9a9ec5
YulinYang-Charlie <42225955+YulinYang-Charlie@users.noreply.github.com>
2021-05-19 18:43:15 +1000

    update realtime with dataprocessing

--- data_harvester/Twitter_realtime.py
+++ data_harvester/Twitter_realtime.py
@@ -8,6 +8,10 @@ import threading
 from tweepy.streaming import StreamListener
 import time
 
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+from textblob import TextBlob
+
 
 # apis = [{'consumer_key' : 'ZYS7ukLD70hVtrFQd8AOnu56M',
 # 'consumer_secret' : 'xpDdcHBDx1byBia1S1nkWg3ZiEme8AMahhxUJGqAaO6XtjcQ3l',
@@ -20,9 +24,9 @@ import time
 
 # boundary = "140.961681984, -39.159189527500004, 149.976679008, -33.9806475865" # Victoria
 # boundary = "96.81676569599999, -43.740509603, 159.109219008, -9.142175977" # Australia
-# boundary = [96.81676569599999, -43.740509603, 159.109219008, -9.142175977]
-boundary = [140.961681984, -39.159189527500004, 149.976679008, -33.9806475865] # Victoria
-id_lst = []
+boundary = [96.81676569599999, -43.740509603, 159.109219008, -9.142175977] # Australia
+# boundary = [140.961681984, -39.159189527500004, 149.976679008, -33.9806475865] # Victoria
+# id_lst = []
 
 
 
@@ -37,8 +41,8 @@ class Twitter_Stream(StreamListener):
     
     def process_data(self, raw_data):
         data = json.loads(raw_data)
-        id_lst.append(data['user']['id'])
-        self.db.save(data)
+        # id_lst.append(data['user']['id'])
+        self.db.save(dataprocess(data))
 
 
     def on_error(self, status_code):
@@ -58,34 +62,99 @@ def get_args():
     
     return args
 
+def dataprocess(data):
+    ans = {}
+    ans["tweet_id"] = data["id"]
+    date = data["created_at"].split()
+    month = "0"
+    if date[1] == "Jan":
+        month = "01"
+    elif date[1] == "Feb":
+        month = "02"
+    elif date[1] == "Mar":
+        month = "03"
+    elif date[1] == "Apr":
+        month = "04"
+    elif date[1] == "May":
+        month = "05"
+    elif date[1] == "Jun":
+        month = "06"
+    elif date[1] == "Jul":
+        month = "07"
+    elif date[1] == "Aug":
+        month = "08"
+    elif date[1] == "Sep":
+        month = "09"
+    elif date[1] == "Oct":
+        month = "10"
+    elif date[1] == "Nov":
+        month = "11"
+    else:
+        month = "12"
+        
+    ans["created_at"] = date[-1]+month+date[2]
+    # print(ans["created_at"])
+    ans["text"] = data["text"]
+    ans["user_id"] = data["user"]["id"]
+    ans["username"] = data["user"]["screen_name"]
+    ans["location"] = data["user"]["location"]
+    ans["geo"] = data["geo"]
+    ans["coordinates"] = data["coordinates"]
+    ans["place"] = data["place"]
 
-def tweet_user_timeline(apis, db):
-    i = 0
-    api = apis[0]
-    while True:
-        if(len(id_lst) == 0):
-            continue
 
-        # requesting timeline
-        id = id_lst.pop(0)
+    # analysis = TextBlob(tweet)
+    score = SentimentIntensityAnalyzer().polarity_scores(data["text"])
+    neg = score['neg']
+    neu = score['neu']
+    pos = score['pos']
 
-        try:
-            for tweet in tweepy.Cursor(api.user_timeline, user_id=id).items(100):
-                print(tweet)
-                if "text" in tweet._json:
-                    print('USER: %s -- %s\n' % (tweet._json['user']['screen_name'], tweet._json['text']))
-                    # save tweet to database
-                    db.save(tweet._json)
-                elif 'message' in tweet._json:
-                    print('ERROR %s: %s\n' % (tweet._json['code'], tweet._json['message']))
-        except:
-            i += 1
-            if i == len(apis):
-                i = 0
-            api = apis[i]
-            print("Exceed rate limits, switch to the next api")
-            id_lst.append(id)
-            pass
+    if neg > pos:
+        sentiment = "negative"
+    elif pos > neg:
+        sentiment = "positive"
+
+    elif pos == neg:
+        sentiment = "neutral"
+
+    ans["sentiment"] = sentiment
+
+    for i in ans.keys():
+        if ans[i] == "null":
+            ans[i] = ""
+
+
+
+    return ans
+
+
+# def tweet_user_timeline(apis, db):
+#     i = 0
+#     api = apis[0]
+#     while True:
+#         if(len(id_lst) == 0):
+#             continue
+
+#         # requesting timeline
+#         id = id_lst.pop(0)
+
+#         try:
+#             for tweet in tweepy.Cursor(api.user_timeline, user_id=id).items(100):
+#                 print(tweet)
+#                 if "text" in tweet._json:
+#                     print('USER: %s -- %s\n' % (tweet._json['user']['screen_name'], tweet._json['text']))
+#                     # save tweet to database
+#                     db.save(tweet._json)
+#                 elif 'message' in tweet._json:
+#                     print('ERROR %s: %s\n' % (tweet._json['code'], tweet._json['message']))
+#         except:
+#             i += 1
+#             if i == len(apis):
+#                 i = 0
+#             api = apis[i]
+#             print("Exceed rate limits, switch to the next api")
+#             id_lst.append(id)
+#             pass
 
 def main():
     args = get_args()
@@ -108,7 +177,7 @@ def main():
         db = couch[args.db]
     
     # t1 = threading.Thread(target=tweet_realtime, args=(apis[0], db, boundary))
-    t2 = threading.Thread(target=tweet_user_timeline, args=(apis[1:], db))
+    # t2 = threading.Thread(target=tweet_user_timeline, args=(apis[1:], db))
 
     # t1.start()
     t2.start()
