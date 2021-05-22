c0ac15a6a363a1c02432a57f263cbbae8de7f989
c348629 <qingxuan@student.unimelb.edu.au>
2021-05-22 21:34:35 +0800

    pie page style

--- frontend/src/pages/PiePage.js
+++ frontend/src/pages/PiePage.js
@@ -1,87 +1,101 @@
 import React from "react";
 import PieChart from "../charts/Pie";
 
-class PiePage extends React.Component{
-    constructor (props) {
-      super(props);
-      this.state = {
-        show: false,
-        value: '',
-        keywords: ['---', 'mask', 'covid', 'pandemic', 'lockdown'],
-        areas: ['---', 'all', 'victoria', 'new south wales', 'queensland', 'tasmania', 
-        'western australia', 'northern territories', 'south australia'],
-      };
-      this.showChart = this.showChart.bind(this);
-    }
-    
-    getValueKeyword=(event)=>{
-      //获取被选中的值
-      console.log('keyword: ' + event.target.value);
-      this.setState({
-        //默认值改变
-        selectKeyword:event.target.value
-      })
-      // alert("keyword changed: " + event.target.value);
+class PiePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false,
+            value: '',
+            keywords: ['---', 'mask', 'covid', 'pandemic', 'lockdown'],
+            areas: ['---', 'all', 'victoria', 'new south wales', 'queensland', 'tasmania',
+            'western australia', 'northern territories', 'south australia'],
+        };
+        this.showChart = this.showChart.bind(this);
     }
 
-    getValueArea=(event)=>{
-      //获取被选中的值
-      console.log('Area: ' + event.target.value);
-      this.setState({
-        //默认值改变
-        selectArea:event.target.value
-      })
-      // alert("area changed: " + event.target.value);
+    getValueKeyword = (event) => {
+        //获取被选中的值
+        console.log('keyword: ' + event.target.value);
+        this.setState({
+            //默认值改变
+            selectKeyword: event.target.value
+        })
     }
 
-    showChart(){
-      this.setState({
-        show: true,
-        params: {
-          keyword: this.state.selectKeyword,
-          area: this.state.selectArea,
-        }
-      });
+    getValueArea = (event) => {
+        //获取被选中的值
+        console.log('Area: ' + event.target.value);
+        this.setState({
+            //默认值改变
+            selectArea: event.target.value
+        })
+    }
+
+    showChart() {
+        this.setState({
+            show: true,
+        });
     }
 
     render() {
-      return (
-        <>
-        <div style={{
-            display: 'flex',
-            marginLeft: '10%',
-        }}>
-          keyword: <select style={{
-                marginRight: '10%',
-            }} value={this.state.selectKeyword} onChange={(e)=>this.getValueKeyword(e)}>
-          {
-            this.state.keywords.map((ele,index)=>{
-              return(
-                <option key={index}>{ele}</option>
-              )
-            })
-          }
-          </select>
-          area: <select style={{
-                marginRight: '10%',
-            }} value={this.state.selectArea} onChange={(e)=>this.getValueArea(e)}>
-          {
-            this.state.areas.map((ele,index)=>{
-              return(
-                <option key={index}>{ele}</option>
-              )
-            })
-          }
-          </select>
-          <button onClick={this.showChart}>click to submit</button>
-        </div>
-        {
-          this.state.show?(
-            <div> <PieChart param={this.state.params}/> </div>
-          ):null
-        }
-        </>
-      );
+        return (
+            <>
+                <div style={{
+                    padding: '50px',
+                    margin: 'auto',
+                }}>
+                    <div style={{
+                        display: 'flex',
+                        paddingLeft: '20%',
+                        paddingRight: '20%',
+                        justifyContent: 'center',
+                        margin: 'auto',
+                    }}>
+                        keyword: <select style={{
+                        marginRight: '10%',
+                    }} value={this.state.selectKeyword} onChange={(e) => this.getValueKeyword(e)}>
+                        {
+                            this.state.keywords.map((ele, index) => {
+                                return (
+                                    <option key={index}>{ele}</option>
+                                )
+                            })
+                        }
+                    </select>
+                        area: <select style={{
+                        marginRight: '10%',
+                    }} value={this.state.selectArea} onChange={(e) => this.getValueArea(e)}>
+                        {
+                            this.state.areas.map((ele, index) => {
+                                return (
+                                    <option key={index}>{ele}</option>
+                                )
+                            })
+                        }
+                    </select>
+                        <button onClick={this.showChart}>click to submit</button>
+
+                    </div>
+                    {
+                        this.state.show ? (
+
+                            <div style={{
+                                height: '50%',
+                                width: '50%',
+                                margin: 'auto',
+                                top: '20px',
+                            }}>
+                                <div>
+                                    <PieChart param={this.state.params}/>
+                                </div>
+                            </div>
+
+                        ) : null
+                    }
+                </div>
+            </>
+        );
     }
 }
 
