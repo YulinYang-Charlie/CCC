d3ff5dd1ead9c13390216d6a0fe3f34cb99a729d
c348629 <qingxuan@student.unimelb.edu.au>
2021-05-22 23:51:45 +0800

    add select picker and update style to pie page

---
+++ frontend/src/lib/Selections.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+export const keywords = [
+    {
+        "label": "Mask",
+        "value": "Mask",
+        "role": "Master"
+    },
+    {
+        "label": "Covid",
+        "value": "Covid",
+        "role": "Master"
+    },
+    {
+        "label": "Pandemic",
+        "value": "Pandemic",
+        "role": "Master"
+    },
+    {
+        "label": "Lockdown",
+        "value": "Lockdown",
+        "role": "Master"
+    }]
+
+export const areas = [
+    {
+        "label": "All",
+        "value": "All",
+        "role": "Master"
+    },
+    {
+        "label": "Victoria",
+        "value": "victoria",
+        "role": "Master"
+    },
+    {
+        "label": "New South Wales",
+        "value": "New South Wales",
+        "role": "Master"
+    },
+    {
+        "label": "Queensland",
+        "value": "Queensland",
+        "role": "Master"
+    },
+    {
+        "label": "Tasmania",
+        "value": "Tasmania",
+        "role": "Master"
+    },
+    {
+        "label": "Western Australia",
+        "value": "Western Australia",
+        "role": "Master"
+    },
+    {
+        "label": "Northern Territories",
+        "value": "Northern Territories",
+        "role": "Master"
+    },
+    {
+        "label": "South Australia",
+        "value": "South Australia",
+        "role": "Master"
+    },
+]
